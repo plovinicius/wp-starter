28fb6762fd120c3f7dd3878c40bd1151eb0f3bef
Paulo Vinicius <ploviniciusplo@gmail.com>
2021-01-19 00:24:47 -0300

    feat: add custom sizes support

---
+++ app/functions/images.php
@@ -0,0 +1,9 @@
+<?php
+
+if (function_exists('add_theme_support')) {
+  // Add Thumbnail Theme Support
+  add_theme_support('post-thumbnails');
+
+  // Custom sizes
+  // add_image_size('custom', 250, '', true);
+}
